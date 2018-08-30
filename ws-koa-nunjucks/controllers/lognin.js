10b29227d18065875b1cd834872bd663bc6b6108
quan.nikolas <quan.nikolas@gmail.com>
2018-08-30 23:08:41 +0800

    解决看不到返回体的问题.

--- ws-koa-nunjucks/controllers/lognin.js
+++ ws-koa-nunjucks/controllers/lognin.js
@@ -3,6 +3,7 @@
 module.exports = {
   "GET /lognin": async (ctx, next) => {
     console.log("我被请求了!");
-    ctx.response.body = "您好,您成功登录了!";
+    ctx.response.body = { msg: "请求成功了." };
+    /**返回体需要使用text() 方法或者json()方法解析 */
   }
 };
